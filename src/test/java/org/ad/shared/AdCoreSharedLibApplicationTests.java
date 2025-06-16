9bf6eedb31140c6782b0d48ce8b326c4d1287d23
giyeon <devgiyeon@gmail.com>
2025-06-16 16:14:27 +0900

    :sparkles: feat: 프로젝트 dependencies 세팅

---
+++ src/test/java/org/ad/shared/AdCoreSharedLibApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ad.shared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdCoreSharedLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
